3e97fb2db6d1a70881cd6a1c124d228528914532
Pedro RBC <pedrinhorbc@gmail.com>
2023-01-20 17:42:10 +0000

    Aula-2

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,17 @@
 import './styles/global.css'
 
-import { Habit } from './components/Habit'
-function App() {
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 
-  return (<div>
-    <Habit title='1' />
-    <Habit title='2' />
-    <Habit title='3' />
-    <Habit title='4' />
-    
+export function App() {
 
-  </div>
-
+  return (
+    <div className='w-screen h-screen flex justify-center items-center ' >
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16 '>
+        <Header />
+        <SummaryTable/>
+      </div>
+    </div>
     )
   }
-    
-export default App
+
