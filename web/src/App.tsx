66310e99ddeeb7421cf5869f962711a555eed10f
Pedro RBC <pedrinhorbc@gmail.com>
2023-01-20 13:49:51 +0000

    Aula-1

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+import './styles/global.css'
+
+import { Habit } from './components/Habit'
+function App() {
+
+  return (<div>
+    <Habit title='1' />
+    <Habit title='2' />
+    <Habit title='3' />
+    <Habit title='4' />
+    
+
+  </div>
+
+    )
+  }
+    
+export default App
